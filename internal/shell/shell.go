44d48570d175f2795adc1358dea0d3bb06fb417a
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2023-02-12 23:14:06 -0800

    Revert "feat: add a few suggestions"
    This reverts commit 14b578add4ffa2cdf2e43f27fd6c790f27cf1cf1.

--- internal/shell/shell.go
+++ internal/shell/shell.go
@@ -13,6 +13,9 @@ import (
 // https://hackernoon.com/today-i-learned-making-a-simple-interactive-shell-application-in-golang-aa83adcb266a
 
 func Run() {
+	// suggests := getCommandSuggests()
+	suggests := []prompt.Suggest{}
+
 	// https://github.com/c-bata/kube-prompt/blob/fa7ef4fe0000bbeef8a2b0e022d23f44d83af5b7/main.go#L33-L41
 	prompt.New(
 		func(command string) {
@@ -20,12 +23,41 @@ func Run() {
 				fmt.Fprintln(os.Stderr, err)
 			}
 		},
-		createCompleter().complete,
+		func(d prompt.Document) []prompt.Suggest {
+			currentWord := d.GetWordBeforeCursor()
+
+			return prompt.FilterHasPrefix(suggests, currentWord, true)
+		},
 		prompt.OptionTitle("go-shell: interactive shell"),
 		prompt.OptionPrefix("$ "),
 		prompt.OptionInputTextColor(prompt.Yellow),
 		prompt.OptionCompletionWordSeparator(completer.FilePathCompletionSeparator),
 	).Run()
+}
+
+func getCommandSuggests() []prompt.Suggest {
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	commands := []string{}
+	for _, path := range paths {
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			fmt.Print("debug: failed to read a dir: " + path + ". error: ")
+			fmt.Println(err)
+			continue
+		}
+		for _, entry := range entries {
+			commands = append(commands, entry.Name())
+		}
+	}
+	suggests := make([]prompt.Suggest, len(commands))
+	for index, command := range commands {
+		suggests[index] = prompt.Suggest{
+			Text: command,
+			// Description: "",
+		}
+	}
+
+	return suggests
 }
 
 func runCommand(commandStr string) error {
