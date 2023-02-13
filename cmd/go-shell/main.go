11a3f36ece2fe4f16d6efec1cfd554c8ecb90da6
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2023-02-12 19:43:31 -0800

    Initial commit

---
+++ cmd/go-shell/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/at-ishikawa/go-shell/internal/shell"
+)
+
+func main() {
+    shell.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/at-ishikawa/go-shell
+
+go 1.19
